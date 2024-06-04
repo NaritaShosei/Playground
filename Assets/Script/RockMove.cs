fa2da448762422a9c2fa0023b8c7de8016a18057
NaritaShosei <onigirikun7171@gmail.com>
2024-06-04 13:28:03 +0900

    石が破壊された時のエフェクトを追加

--- Assets/Script/RockMove.cs
+++ Assets/Script/RockMove.cs
@@ -8,21 +8,23 @@ public class RockMove : MonoBehaviour
     [SerializeField] float _rockSpeed = 1f;
     [Header("石が破壊される時間")]
     [SerializeField] float _destroyTime = 1f;
+    [Header("石が破壊されるときのエフェクト")]
+    [SerializeField] GameObject _effectPrefub;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D _rockrb = GetComponent<Rigidbody2D>(); 
         _rockrb.velocity = new Vector2(0,-_rockSpeed);
-        Invoke("Destroy",_destroyTime);
+        Invoke("Destroy", _destroyTime);
     }
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "Player")
-    //    {
-    //        GetComponent<AudioSource>().Play();
-    //        Destroy(this.gameObject);
-    //    }
-    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject effect = Instantiate(_effectPrefub, this.transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+        }
+    }
     private void Destroy()
     {
         Destroy(gameObject);
